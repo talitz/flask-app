3406db6c4a0e12c2f5230bd52c086dcd0082ac5b
Tal Yitzhak <taly@lightrun.com>
2024-08-30 13:53:33 +0300

    fixed terraform pipeline

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.65.0"
+    }
+  }
+}
+
 provider "aws" {
   shared_credentials_files = ["$HOME/.aws/credentials"]
   region = var.region
