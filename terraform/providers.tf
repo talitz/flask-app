8fffaa5ba804067e2035c43df9bccd1243c3d73e
Tal Yitzhak <taly@lightrun.com>
2024-08-31 11:07:31 +0300

    latest terraform changes

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,6 +5,20 @@ terraform {
       version = "5.65.0"
     }
   }
+  backend "s3" {
+    bucket = "terraform-state-flask-app-infra"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
+  }
+}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-state-flask-app-infra"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
+  }
 }
 
 provider "aws" {
